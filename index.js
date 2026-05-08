c66879e65aa72b22403343be312e93b78999ba0d
yu-nykanorova <juliyasos@ukr.net>
2026-05-08 20:39:28 +0300

    completed homework 3

--- index.js
+++ index.js
@@ -1,54 +1,64 @@
 const express = require('express');
+const fsPromises = require('node:fs/promises');
+const path = require('node:path');
+const {getData, validateData} = require("./helpers/helpers");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const users = [
-    {id: 1, name: "Maksym", email: "feden@gmail.com", password: "qwe123"},
-    {id: 2, name: "Alina", email: "alindosik@gmail.com", password: "ert345"},
-    {id: 3, name: "Anna", email: "ann43@gmail.com", password: "ghj393123"},
-    {id: 4, name: "Tamara", email: "tomochka23@gmail.com", password: "afs787"},
-    {id: 5, name: "Dima", email: "taper@gmail.com", password: "rtt443"},
-    {id: 6, name: "Rita", email: "torpeda@gmail.com", password: "vcx344"},
-    {id: 7, name: "Denis", email: "denchik@gmail.com", password: "sdf555"},
-    {id: 8, name: "Sergey", email: "BigBoss@gmail.com", password: "ccc322"},
-    {id: 9, name: "Angela", email: "lala@gmail.com", password: "cdd343"},
-    {id: 10, name: "Irina", email: "irka7@gmail.com", password: "kkk222"}
-];
+const pathToFile = path.join(__dirname, 'users.json');
 
-// app.get('/users', (req, res) => {
-//     res.send('Get users');
-// });
-
-app.get('/users', (req, res) => {
+app.get('/users', async (req, res) => {
     try {
+        const users = await getData(pathToFile);
+
         res.send(users);
     } catch (e) {
         res.status(500).send(e.message);
     }
 });
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     try {
         const {name, email, password} = req.body;
-        // TODO validate data
+
+        const validationResult = validateData(name, email, password);
+
+        if (validationResult) {
+            return res.status(400).send(validationResult);
+        }
+
+        const users = await getData(pathToFile);
+
+        if (users.find((user) => user.email === email)) {
+            return res.status(409).send("This email is already in use.");
+        }
+
         const id = users[users.length - 1].id + 1
         const newUser = {id, name, email, password};
         users.push(newUser);
+
+        await fsPromises.writeFile(pathToFile, JSON.stringify(users), 'utf8');
+
         res.status(201).send(newUser);
 
-        // console.log(req.params);
-        // console.log(req.query);
     } catch (e) {
         res.status(500).send(e.message);
     }
 });
 
-app.get('/users/:userId', (req, res) => {
+app.get('/users/:userId', async (req, res) => {
     try {
         const userId = Number(req.params.userId);
+
+        if (Number.isNaN(userId)) {
+            return res.status(400).send("User must be an integer");
+        }
+
+        const users = await getData(pathToFile);
+
         const user = users.find((user) => user.id === userId);
         if (!user) {
             return res.status(404).send("User not found");
@@ -59,33 +69,58 @@ app.get('/users/:userId', (req, res) => {
     }
 });
 
-app.put('/users/:userId', (req, res) => {
+app.put('/users/:userId', async (req, res) => {
     try {
         const userId = Number(req.params.userId);
+
+        if (Number.isNaN(userId)) {
+            return res.status(400).send("User must be an integer");
+        }
+
+        const users = await getData(pathToFile);
+
         const userIndex = users.findIndex((user) => user.id === userId);
         if (userIndex === -1) {
             return res.status(404).send("User not found");
         }
         const {name, email, password} = req.body;
-        // TODO validate data
-        // users[userIndex] = {...users[userIndex], name, email, password};
+
+        const validationResult = validateData(name, email, password);
+
+        if (validationResult) {
+            return res.status(400).send(validationResult);
+        }
+
         users[userIndex].name = name;
         users[userIndex].email = email;
         users[userIndex].password = password;
+
+        await fsPromises.writeFile(pathToFile, JSON.stringify(users), 'utf8');
+
         res.status(201).send(users[userIndex]);
     } catch (e) {
         res.status(500).send(e.message);
     }
 });
 
-app.delete('/users/:userId', (req, res) => {
+app.delete('/users/:userId', async (req, res) => {
     try {
         const userId = Number(req.params.userId);
+
+        if (Number.isNaN(userId)) {
+            return res.status(400).send("User must be an integer");
+        }
+
+        const users = await getData(pathToFile);
+
         const  userIndex = users.findIndex((user) => user.id === userId);
         if (userIndex === -1) {
             return res.status(404).send('No such user');
         }
         users.splice(userIndex, 1);
+
+        await fsPromises.writeFile(pathToFile, JSON.stringify(users), 'utf8');
+
         res.sendStatus(204);
     } catch (e) {
         res.status(500).send(e.message);
