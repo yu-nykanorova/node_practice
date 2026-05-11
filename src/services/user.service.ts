c7292e89f3b955abce3e545ff775249c967fa519
yu-nykanorova <juliyasos@ukr.net>
2026-05-11 17:25:09 +0300

    restructured to follow the example of a layered backend architectureÐ

---
+++ src/services/user.service.ts
@@ -0,0 +1,51 @@
+import { ApiError } from "../errors/api-error";
+import { IUser } from "../interfaces/user.interface";
+import { userRepository } from "../repositories/user.repository";
+
+class UserService {
+  public async getList(): Promise<IUser[]> {
+    return await userRepository.getList();
+  }
+
+  public async create(dto: Partial<IUser>): Promise<IUser> {
+    if (!dto.name || dto.name.length < 3) {
+      throw new ApiError(
+        "Name is required and should be at least 3 characters long",
+        400,
+      );
+    }
+    if (!dto.email || !dto.email.includes("@")) {
+      throw new ApiError("Email is required and should be valid", 400);
+    }
+    if (!dto.password || dto.password.length < 6) {
+      throw new ApiError(
+        "Password is required and should be at least 6 characters long",
+        400,
+      );
+    }
+
+    const users = await userRepository.getList();
+
+    if (users.find((user) => user.email === dto.email)) {
+      throw new ApiError("This email is already in use", 409);
+    }
+
+    return await userRepository.create(dto);
+  }
+
+  public async getById(userId: number): Promise<IUser> {
+    if (Number.isNaN(userId)) {
+      throw new ApiError("User id must be an integer", 400);
+    }
+
+    const user = await userRepository.getById(userId);
+
+    if (!user) {
+      throw new ApiError("User not found", 404);
+    }
+
+    return user;
+  }
+}
+
+export const userService = new UserService();
